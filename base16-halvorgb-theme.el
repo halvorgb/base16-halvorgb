8dfec661f6ba3eaa9269ea0e63f0fe50e186fba0
halvor gb <halvorgb@gmail.com>
2014-01-16 13:20:02 +0100

    .

--- base16-halvorgb-theme.el
+++ base16-halvorgb-theme.el
@@ -32,12 +32,12 @@
       (base07 "#f2f0ec")
       (base08 "#f2777a")
       (base09 "#f99157")
-      (base0A: "#ffcc66")
-      (base0B: "#99cc99")
-      (base0C: "#66cccc")
-      (base0D: "#6699cc")
-      (base0E: "#cc99cc")
-      (base0F: "#d27b53")
+      (base0A "#ffcc66")
+      (base0B "#99cc99")
+      (base0C "#66cccc")
+      (base0D "#6699cc")
+      (base0E "#cc99cc")
+      (base0F "#d27b53")
 
       )
 
@@ -45,68 +45,69 @@
    'base16-halvorgb
 
    ;; Built-in stuff (Emacs 23)
-   `(default ((t (:background ,background :foreground ,foreground))))
-   `(fringe ((t (:background ,current-line))))
-   `(minibuffer-prompt ((t (:foreground ,blue))))
-   `(mode-line ((t (:background ,current-line :foreground ,foreground))))
-   `(region ((t (:background ,selection))))
+   `(default ((t (:background, base00 :foreground, base07))))
+   `(fringe ((t (:background, base00))))
+   `(minibuffer-prompt ((t (:foreground, base0D))))
+   `(mode-line ((t (:background, base00 :foreground, base07))))
+   `(region ((t (:background, base01)))) ;; selection
+   `(cursor ((t (:background, base07))))
 
    ;; Font-lock stuff
-   `(font-lock-comment-face ((t (:foreground ,comment))))
-   `(font-lock-constant-face ((t (:foreground ,aqua))))
-   `(font-lock-doc-string-face ((t (:foreground ,comment))))
-   `(font-lock-function-name-face ((t (:foreground ,blue))))
-   `(font-lock-keyword-face ((t (:foreground ,red))))
-   `(font-lock-string-face ((t (:foreground ,green))))
-   `(font-lock-type-face ((t (:foreground ,purple))))
-   `(font-lock-variable-name-face ((t (:foreground, yellow))))
-   `(font-lock-warning-face ((t (:foreground ,red))))
-   `(font-lock-negation-char-face ((t (:foreground, orange))))
-   `(font-lock-doc-face ((t (:foreground, comment))))
+   `(font-lock-comment-face ((t (:foreground, base03))))
+   `(font-lock-doc-face ((t (:background, base00 :foreground, base04))))
+   `(font-lock-constant-face ((t (:foreground, base09))))
+   `(font-lock-function-name-face ((t (:foreground, base0D))))
+   `(font-lock-keyword-face ((t (:foreground, base08))))
+   `(font-lock-string-face ((t (:background, base02 :foreground, base0B))))
+   `(font-lock-type-face ((t (:foreground, base0A))))
+   `(font-lock-variable-name-face ((t (:foreground, base0E))))
+   `(font-lock-warning-face ((t (:foreground, base08))))
+   `(font-lock-negation-char-face ((t (:foreground, base0F))))
+
 
    ;; hl-line-mode
-   `(hl-line ((t (:background ,current-line))))
+   `(hl-line ((t (:background, base00))))
 
    ;; linum-mode
-   `(linum ((t (:background ,current-line :foreground ,foreground))))
+   `(linum ((t (:background, base00 :foreground, base07))))
 
    ;; org-mode
-   `(org-date ((t (:foreground ,purple))))
-   `(org-done ((t (:foreground ,green))))
-   `(org-hide ((t (:foreground ,current-line))))
-   `(org-link ((t (:foreground ,blue))))
-   `(org-todo ((t (:foreground ,red))))
+   `(org-date ((t (:foreground, base0E))))
+   `(org-done ((t (:foreground, base0B))))
+   `(org-hide ((t (:foreground, base00))))
+   `(org-link ((t (:foreground, base0D))))
+   `(org-todo ((t (:foreground, base08))))
 
    ;; IDO
-   `(ido-only-match ((t (:foreground ,red))))
-   `(ido-subdir ((t (:foreground, green))))
-   `(ido-first-match ((t (:foreground, yellow))))
+   `(ido-only-match ((t (:foreground, base08))))
+   `(ido-subdir ((t (:foreground, base0B))))
+   `(ido-first-match ((t (:foreground, base0A))))
 
 
    ;; show-paren-mode
-   `(show-paren-match ((t (:background ,blue :foreground ,current-line))))
-   `(show-paren-mismatch ((t (:background ,orange :foreground ,current-line))))
+;;   `(show-paren-match ((t (:background, blue :foreground ,current-line))))
+;;   `(show-paren-mismatch ((t (:background ,orange :foreground ,current-line))))
 
    ;; rainbow-delimiters
-   `(rainbow-delimiters-depth-1-face ((t (:foreground ,purple))))
-   `(rainbow-delimiters-depth-2-face ((t (:foreground ,blue))))
-   `(rainbow-delimiters-depth-3-face ((t (:foreground ,aqua))))
-   `(rainbow-delimiters-depth-4-face ((t (:foreground ,green))))
-   `(rainbow-delimiters-depth-5-face ((t (:foreground ,yellow))))
-   `(rainbow-delimiters-depth-6-face ((t (:foreground ,orange))))
-   `(rainbow-delimiters-depth-7-face ((t (:foreground ,red))))
-   `(rainbow-delimiters-depth-8-face ((t (:foreground ,comment))))
-   `(rainbow-delimiters-depth-9-face ((t (:foreground ,foreground)))))
-
+   ;; `(rainbow-delimiters-depth-1-face ((t (:foreground ,purple))))
+   ;; `(rainbow-delimiters-depth-2-face ((t (:foreground ,blue))))
+   ;; `(rainbow-delimiters-depth-3-face ((t (:foreground ,aqua))))
+   ;; `(rainbow-delimiters-depth-4-face ((t (:foreground ,green))))
+   ;; `(rainbow-delimiters-depth-5-face ((t (:foreground ,yellow))))
+   ;; `(rainbow-delimiters-depth-6-face ((t (:foreground ,orange))))
+   ;; `(rainbow-delimiters-depth-7-face ((t (:foreground ,red))))
+   ;; `(rainbow-delimiters-depth-8-face ((t (:foreground ,comment))))
+   ;; `(rainbow-delimiters-depth-9-face ((t (:foreground ,foreground)))))
+   )
   (custom-theme-set-variables
    'base16-halvorgb
 
    `(ansi-color-names-vector
      ;; black, red, green, yellow, blue, magenta, cyan, white
-     [,background ,red ,green ,yellow ,blue ,purple ,blue ,foreground])
+     [,base00, base08, base0B, base0A, base0D, base0E, base0C, base07])
    `(ansi-term-color-vector
      ;; black, red, green, yellow, blue, magenta, cyan, white
-     [unspecified ,background ,red ,green ,yellow ,blue ,purple ,blue ,foreground])))
+     [unspecified, base00, base08, base0B, base0A, base0D, base0E, base0C, base07])))
 
 (provide-theme 'base16-halvorgb)
 
